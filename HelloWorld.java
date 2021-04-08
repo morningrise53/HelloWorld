99bb2e91d378894c2fde4998e1b916546cabb6bc
morningrise53 <82133777+morningrise53@users.noreply.github.com>
2021-04-08 00:25:01 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,11 +5,12 @@
 * Description: This is my first program
 */
 
-
 class HelloWorld{
     public static void main(String[] args) {
+        
         // This is a comment. This means the compiler skips this line
         // This command prints out Hello World into the console
         System.out.println("Hello World");
+        System.out.println("Fuck off");
     }
 }
