bb9e8429be94be8ffef67fc69cb0f6cf1a5e08b7
morningrise53 <82133777+morningrise53@users.noreply.github.com>
2021-04-08 00:23:15 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+* Date: Feb 16, 2021
+* Name: Amir
+* Teacher: Mr. Ho
+* Description: This is my first program
+*/
+
+
+class HelloWorld{
+    public static void main(String[] args) {
+        // This is a comment. This means the compiler skips this line
+        // This command prints out Hello World into the console
+        System.out.println("Hello World");
+    }
+}
